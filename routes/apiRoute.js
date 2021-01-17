a046f61993fc784259e27eb71ba924ce64f21847
Matt Harris <711mattman@gmail.com>
2021-01-17 14:07:10 -0500

    Added the get and post fies so far

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,3 +1,27 @@
 var express = require("express");
 var router = express.Router();
 const Workout = require("../models/workout.js");
+
+module.exports = function(app) {
+    router.use(function timeLog(req, res, next) {
+        console.log("Time: ", Date.now());
+        next();
+    });
+
+    router.get("/api/workouts", (req, res) => {
+        Workout.find()
+        .then(dbWorkout => {
+            res.join(dbWorkout);
+        }).catch(err => {
+            res.join(err);
+        });
+    });
+    router.post("/api/workouts", (req, res) => {
+        Workout.create({}).then(dbWorkout => {
+            res.join(dbWorkout);
+        })
+        .catch(err => {
+            res.join(err);
+        });
+    });
+}
