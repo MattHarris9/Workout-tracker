019d876cfbb6541abae780ddb4c8f0e4354fab48
Matt Harris <711mattman@gmail.com>
2021-01-17 13:40:14 -0500

    Made the schema for the workout tracker and exported it

--- models/workout.js
+++ models/workout.js
@@ -14,7 +14,39 @@ const workoutSchema = new Schema({
                 trim: true,
                 required: "Enter your exercise type"
             },
-            
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter your exercise name"
+            },
+            duration: {
+                type: Number,
+                required: "Enter your exercise time in minutes"
+            },
+            disttance: {
+                type: Number
+            },
+            reps:{
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            weights: {
+                type: Number
+            }
+
+
         }
     ]
-})
+});
+
+workoutSchema.virtual("totalDuration").get(function() {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0)
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports= Workout;
